ff91c9203617483279066abb8381718a88f05f41
abdelrhman <abdelrhmanatwa39@gmail.com>
2023-08-07 23:03:53 +0200

    session Code and Task

---
+++ main.js
@@ -0,0 +1,125 @@
+
+
+// var loading=true;
+// var data;
+
+
+// (!data)??loading==false;
+
+// if(!data){
+
+// }else{
+//     loading==false;
+// }
+
+// if(loading == true){
+//     'display:block';
+// }else{
+//     'display:none';
+// }
+
+// var myData =``;
+// alert(myData);
+
+
+// myData = data??'false'
+
+// if(data){
+//     data
+// }else{
+//     'false'
+// }
+
+// alert(myData);
+// ====================== End var If , else
+
+// var , Let , Const
+// const x = 11;
+
+// if(x == 11){
+//     let h ='Hello';
+// }
+// console.log(i);
+
+// &=and
+// || = or
+
+// name ='ali'
+// age =18
+
+// name ='ahmed' & age= 18
+// name ='ahmed' || age= 18
+
+
+// ==================== switch ====================
+// var name = 'ali';
+
+// if(name=='mohamed'){
+//     alert('mohamed')
+// }else{
+//     alert('false');
+// }
+
+
+// switch (name) {
+//     case 'mohamed':
+//         alert('mohamed')
+//         break;
+//     case 'ali':
+//         alert('ali')
+//         break;
+//     case 'mahmoud':
+//         alert('mahmoud')
+//         break;
+//     default:
+//         alert('false');
+//         break;
+// }
+
+
+// =========================== loops ==========
+// For , While , Do While , foeach
+
+
+
+// var i =1;
+// ++i Pre => 2
+// i++ Post => 1
+
+// --i
+// i--
+
+// var data = ['ali' , 15 , 18];
+
+
+
+// for (let i = 0; i < 3; i++) {
+//     const element = data[i];
+//     console.log(element);
+// }
+
+
+
+// ========= Task =======
+
+/*
+
+- Task-1
+var data = [1,12,2,56,8,64,46,7,465,8,4,5,54,8,3,5,456,4,8];
+عاوز اطبع الارقام الفرديه بس تحت بعض
+
+- Task-2
+عاوز اطلب الاسم من اليوزر 
+فلو بعت اسمو محمد
+
+اطلع رساله بتقولو 
+(مرحباً محمد)
+
+*/
+
+
+
+
+
+
+
